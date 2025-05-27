34fffbb54d0e2a1c0c650bf8843fb2223e70c64b
Gustavo Pereira Goncalves de Campos <gpereira@sg.unicamp.br>
2025-05-27 10:04:53 -0300

    feat: remove railway.json file and upload index

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <?php
 
-$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-if ($uri !== '/' && file_exists(__DIR__ . $uri)) {
-    return false;
-}
-
 require __DIR__.'/vendor/autoload.php';
 
 use \App\Http\Router;
 use \App\Utils\View;
 
-$scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
-$host = $_SERVER['HTTP_HOST'] ?? 'localhost';
-define('URL', $scheme . '://' . $host);
+define('URL', 'http://mvcestrutural.dev.local');
 
+// define valor padrão para as variáveis
 View::init([
-    'URL'=> URL
+'URL'=> URL
 ]);
 
-$obRouter = new Router(URL);
+$obRouter = new Router(URL);   
 
+//inclui as rotas de páginas
 include __DIR__.'/routes/pages.php';
 
 $obRouter->run()->sendResponse();
